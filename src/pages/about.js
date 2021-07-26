616b67f9cc5d4c4fe34108572852c03f2734f9df
Aliaksei Stadnik <aliaksei.s@svsg.co>
2021-07-26 21:51:20 +0300

    Add page styled componetns

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Layout from '../сcomponent/Layout';
+
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About">
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
+    );
+};
+
+export default AboutPage;
